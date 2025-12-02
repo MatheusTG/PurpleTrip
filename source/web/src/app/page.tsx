91135c037103b1dab190312a9671f0bb2ff9c06e
Matheus <matheusteodorogarcia@gmail.com>
2025-12-02 16:02:51 -0300

    feat: add TripAdd component with image carousel and styles

--- source/web/src/app/page.tsx
+++ source/web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Menu from "@/components/home/menu";
+import TripAdd from "@/components/home/trip-add";
 
 export default function Home() {
   return (
     <article>
       <Menu />
+      <TripAdd />
     </article>
   );
 }
