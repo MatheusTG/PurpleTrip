1fe24cfbd2b15f6ba133afc25f4e949e78912043
Matheus <matheusteodorogarcia@gmail.com>
2025-12-03 11:36:52 -0300

    feat: implement FilterBar component with date range and guest selection

--- source/web/src/app/page.tsx
+++ source/web/src/app/page.tsx
@@ -1,11 +1,16 @@
+import FilterBar from "@/components/home/filter-bar";
 import Menu from "@/components/home/menu";
 import TripAdd from "@/components/home/trip-add";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <article>
       <Menu />
       <TripAdd />
+      <div className={styles.filterBarPosition}>
+        <FilterBar />
+      </div>
     </article>
   );
 }
