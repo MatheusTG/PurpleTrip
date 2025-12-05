5b9a88c34491e6bfc624bc0b2d77490eae14b338
Matheus <matheusteodorogarcia@gmail.com>
2025-12-05 12:01:06 -0300

    feat: implement FilterAside component with filters for price, category, and amenities

--- source/web/src/app/page.tsx
+++ source/web/src/app/page.tsx
@@ -2,14 +2,18 @@ import FilterBar from "@/components/home/filter-bar";
 import Menu from "@/components/home/menu";
 import TripAdd from "@/components/home/trip-add";
 import styles from "./page.module.css";
+import FilterAside from "@/components/home/filter-aside";
 
 export default function Home() {
   return (
-    <article>
+    <article className={styles.homeContainer}>
       <Menu />
       <TripAdd />
-      <div className={styles.filterBarPosition}>
-        <FilterBar />
+      <div className={`${styles.listingContainer} container`}>
+        <div className={styles.filterBarPosition}>
+          <FilterBar />
+        </div>
+        <FilterAside />
       </div>
     </article>
   );
