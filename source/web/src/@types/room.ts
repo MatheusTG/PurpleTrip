1769c4afd5d83178a13ddfefeb86a0e4071e56fc
Matheus <matheusteodorogarcia@gmail.com>
2025-12-05 21:54:52 -0300

    feat: add Room type definition and RoomItem component with styling and functionality

---
+++ source/web/src/@types/room.ts
@@ -0,0 +1,38 @@
+export type Room = {
+  id: string;
+  host: string;
+  title: string;
+  address: {
+    id: string;
+    cep: string;
+    country: string;
+    state: string;
+    city: string;
+    district: string;
+    street: string;
+    number: string;
+    complement: string;
+  };
+  description: string;
+  imagePath: string;
+  category: "casa" | "hotel" | "apartamento";
+  singleBeds: number;
+  doubleBeds: number;
+  dailyPrice: number;
+  cancellationPolicy: "flexible" | "hard";
+  minimumStayDays: number;
+  maximumStayDays: number;
+  blocked: boolean;
+  apartment_number?: string;
+  maximum_guests: number;
+  number_of_single_bed: number;
+  number_of_double_bed: number;
+  policies: {
+    id: string;
+    name: string;
+    icon: string;
+    type: "amenity" | "restriction";
+  }[];
+  rating: number;
+  numberOfReviews: number;
+};
