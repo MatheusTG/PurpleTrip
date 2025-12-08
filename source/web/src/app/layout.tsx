d29ba36d613ed251f56633c615f96619e9305179
natinha <naticamposs010607@gmail.com>
2025-12-08 12:11:40 -0300

    feat: esconde o header e footer no login e adiciona popover do usuário

--- source/web/src/app/layout.tsx
+++ source/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import type { Metadata } from "next";
 import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -27,9 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} ${roboto.className}`}>
-        {/* <Header /> */}
+        <Header />
         <main>{children}</main>
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- source/web/src/components/header/index.tsx
+++ source/web/src/components/header/index.tsx
@@ -1,14 +1,27 @@
-import styles from "./header.module.css";
-import Image from "next/image";
+"use client";
+
 import { MessageCircleQuestionMark } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./header.module.css";
+import UserPopover from "./user-popover";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isAuthPage = ["/login", "/register"].includes(pathname);
+
+  if (isAuthPage) {
+    return null;
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.headerPrincipal}>
         <div className={styles.coluna}>
-          <Image src="/logo.png" alt="Logo" width={173} height={64} />
+          <Link href="/" className={styles.logoContainer}>
+            <Image src="/logo.png" alt="Logo" width={173} height={64} />
+          </Link>
         </div>
 
         <div className={styles.coluna2}>
@@ -17,15 +30,7 @@ export default function Header() {
             <span>Ajuda</span>
           </Link>
 
-          <Link href="#" type="button" className={styles.ajuda}>
-            <Image
-              src="/profile-image.jpg"
-              alt=""
-              width={36}
-              height={36}
-              className={styles.usuario}
-            />
-          </Link>
+          <UserPopover />
         </div>
       </div>
     </header>
