b5b92d5d475465438cbbc9ebf3c249265a25f80f
amandasoaresv <amandasoaresvieira5@gmail.com>
2025-12-09 20:55:55 -0300

    feat: implementa componente de mapa estático e ajusta estilos responsivos

--- source/web/src/components/comentarios/index.tsx
+++ source/web/src/components/comentarios/index.tsx
@@ -1,30 +1,26 @@
 import { Star } from "lucide-react";
 import styles from "./comentarios.module.css";
+import Stars, { StarsAmount } from "@/components/stars";
+import { useState } from "react";
 
 export default function Comentario() {
+  const [nota, setNota] = useState<StarsAmount>(5);
   return (
     <div className={styles.card}>
-      <img
-        src="/profile-image.jpg"   
-        alt="Foto do hóspede"
-        className={styles.foto}
-      />
+      <img src="/profile-image.jpg" alt="Foto do hóspede" className={styles.foto} />
 
       <div>
         <div className={styles.topo}>
           <p className={styles.nome}>Amanda</p>
 
-          <div className={styles.estrelas}>
-            <Star size={16} />
-            <Star size={16} />
-            <Star size={16} />
-            <Star size={16} />
-            <Star size={16} />
+          <div className={styles.estrelasContainer}>
+            <Stars size={22} starsAmount={nota} setStarsAmount={setNota} />
           </div>
         </div>
 
         <p className={styles.texto}>
-          O apartamento é exatamente como nas fotos: limpo, aconchegante e muito bem localizado. Fiquei por 4 dias e tive uma ótima experiência. A cama era confortável, o chuveiro funcionava bem e o Wi-Fi era rápido. 
+          O apartamento é exatamente como nas fotos: limpo, aconchegante e muito bem localizado. Fiquei por 4 dias e
+          tive uma ótima experiência. A cama era confortável, o chuveiro funcionava bem e o Wi-Fi era rápido.
         </p>
       </div>
     </div>
