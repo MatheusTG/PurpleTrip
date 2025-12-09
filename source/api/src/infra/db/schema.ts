603b355399d20f6a49e8f7aa8bf9b590f987481a
Matheus <matheusteodorogarcia@gmail.com>
2025-12-09 14:53:44 -0300

    feat: add type enum to users and add address table

--- source/api/src/infra/db/schema.ts
+++ source/api/src/infra/db/schema.ts
@@ -1,5 +1,6 @@
 import { pgTable, uuid, text, date, boolean, pgEnum, timestamp } from "drizzle-orm/pg-core";
 
+export const profile_type = pgEnum("profile_type", ["QUEST", "HOST", "ADMIN"]);
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
@@ -9,7 +10,18 @@ export const users = pgTable("users", {
   photoPath: text("photo_path"),
   passwordHash: text("password_hash").notNull(),
   isBanned: boolean("is_banned").notNull().default(false),
-  profileType: pgEnum("profile_type", ["QUEST", "HOST", "ADMIN"])().notNull().default("QUEST"),
+  profileType: profile_type("profile_type").notNull().default("QUEST"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const address = pgTable("address", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  cep: text("cep").notNull(),
+  bairro: text("neighborhood").notNull(),
+  street: text("street").notNull(),
+  number: text("number"),
+  complemet: text("number"),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
